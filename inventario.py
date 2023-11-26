bf4c794c64ee80860f767319524c125e5ab45f99
cronwell30 <132957996+cronwell30@users.noreply.github.com>
2023-11-26 01:28:53 -0700

    inventory base
    my inventory project

---
+++ inventario.py
@@ -0,0 +1,41 @@
+products={}
+flag=True
+
+while True:
+  print('''
+    1. Añadir productos.
+    2. Buscar producto.
+    3. Inventario.
+    4. Ver productos.''')
+  
+  respuesta=int(input('Ingrese su opcion: '))
+  
+  if respuesta == 1:
+    print('\t Agregar producto nuevo')
+    name=str(input('Nombre de el producto: '))
+    code=int(input('Codigo de producto: '))
+    quantity=int(input('Cantidad de producto: '))
+    coste=float(input('Precio de venta :$'))
+    products [name]=code,quantity,coste
+    print(products)
+  elif respuesta == 2:
+    search=str(input('Nombre del producto a buscar: '))
+    if search in products:
+      print(f'Detalles de producto {products[search]}')
+  elif respuesta == 3:
+    search=str(input('Producto a inventariar: '))
+    if search in products:
+      contado=[]
+      suma=0
+      while contado != quantity:
+        encontrado=int(input('Cantidad encontrada: '))
+        contado.append(encontrado)
+        print(contado)
+        for i in contado:
+          suma=sum(contado)
+        print(suma)
+        if suma==quantity:
+          terminar=str(input('A encotrado todos los productos deceas terminar (y/n): '))
+          if terminar=='y':
+            print(f'valor de inventario {suma*coste}')
+            break
